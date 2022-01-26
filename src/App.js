3d44ac02580d917e0ea7387823d578faa88841e1
V914eslav <v914-working@rambler.ru>
2022-01-26 12:43:38 +0300

    create PazzaBlock and map him

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 import { Routes, Route } from "react-router-dom";
+import React from "react";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+  React.useEffect(() => {
+    fetch("http://localhost:3000/db.json")
+      .then((resp) => resp.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} exact />
+          <Route path="/" element={<Home items={pizzas} />} exact />
           <Route path="/cart" element={<Cart />} exact />
         </Routes>
       </div>
