f27ad5cad84683d8e52151b806805bec2987c207
V914eslav <v914-working@rambler.ru>
2022-07-19 15:01:19 +0300

    use TypeScript

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useCallback, useState, useRef } from "react";
+import React, { useCallback, useState, useRef } from "react";
 import debounce from "lodash.debounce";
 
 import styles from "./Search.module.scss";
--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   searchValue: "",
-  categoryId: 1,
+  categoryId: 0,
   currentPage: 1,
   sort: {
     name: "популярности",
